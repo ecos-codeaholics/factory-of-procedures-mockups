6e381646e4a12f1f736ea9d45886c34813f2baa8
snaphuman <fhernandezn@gmail.com>
2016-07-22 15:07:49 -0500

    [mockups] improve the way how descriptions are displayed

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,130 +2,147 @@ var data = [];
 
 data["01"] = {
     "file": "01-super-admin-dashboard.png",
+    "title": "Dashboard",
     "desc": "Descripcion" };
 
 data["02"] = {
     "file": "02-super-admin-mayoralty-list.png",
+    "title": "Listar alcaldías",
     "desc": "Descripción"}
 
 data["03"] = {
     "file": "03-super-admin-mayoralty-new.png",
+    "title": "Crear alcaldía",
     "desc": "Descripción"};
 
 data["04"] = {
     "file": "04-super-admin-procedure-list.png",
+    "title": "Listar trámites",
     "desc": "Descripción"};
 
 data["05"] = {
     "file": "05-super-admin-procedure-form-builder.png",
+    "title": "Crear trámite (FormBuilder)",
     "desc": "Descripción"};
 
 data["06"] = {
     "file": "06-super-admin-procedure-form-builder-attribs.png",
+    "title": "Crear trámite (FormBuilder, Field Attributes)",
     "desc": "Descripción"};
 
 data["07"] = {
     "file": "07-super-admin-procedure-form-builder-validate.png",
+    "title": "Crear trámite, Field Validate",
     "desc": "Descripción"};
 
 data["08"] = {
     "file": "08-super-admin-procedure-flow-builder-activities.png",
-    "desc": "Descripción"};
-
-data["09"] = {
-    "file": "08-super-admin-procedure-flow-builder-activities.png",
+    "title": "FlowBuilder, Activities",
     "desc": "Descripción"};
 
 data["09"] = {
     "file": "09-super-admin-procedure-flow-builder-assignee.png",
+    "title": "FlowBuilder, Assignment",
     "desc": "Descripción"};
 
 data["10"] = {
     "file": "10-super-admin-user-role-set.png",
+    "titiel": "Asignar privilegios a usuarios",
     "desc": "Descripción"};
 
 data["11"] = {
     "file": "11-super-admin-user-list-admins.png",
+    "title": "Listar usuarios administradores",
     "desc": "Descripción"};
 
 data["12"] = {
     "file": "12-admin-dashboard.png",
+    "title": "Dashboard administrador",
     "desc": "Descripción"};
 
 data["13"] = {
     "file": "13-admin-mayoralty-edit.png",
+    "title": "Editar Alcaldía",
     "desc": "Descripción"};
 
 data["14"] = {
     "file": "14-admin-procedures-list.png",
+    "title": "Listar trámites pendientes",
     "desc": "Descripción"};
 
 data["15"] = {
     "file": "15-admin-user-list-functionary.png",
+    "title": "Listar funcionarios alcaldia",
     "desc": "Descripción"};
 
 data["16"] = {
     "file": "16-functionary-dashboard.png",
+    "title": "Dashboard funcionario",
     "desc": "Descripción"};
 
 data["16.1"] = {
     "file": "16.1-functionary-dashboard-notification.png",
+    "title": "Dashboard funcionario, notificación de actividad",
     "desc": "Descripción"};
 
 data["17"] = {
     "file": "17-functionary-procedure-pending-list.png",
+    "title": "Listar trámites pendientes",
     "desc": "Descripción"};
 
 data["18"] = {
     "file": "18-functionary-procedure-review-info.png",
+    "title": "Ver información trámite radicado",
     "desc": "Descripción"};
 
 data["19"] = {
     "file": "19-functionary-procedure-review-requirements.png",
+    "title": "Ver requerimientos trámite radicado",
     "desc": "Descripción"};
 
 data["20"] = {
     "file": "20-functionary-procedure-review-flow.png",
-    "desc": "Descripción"};
-
-data["21"] = {
-    "file": "21-functionary-procedure-review-hostory.png",
+    "title": "Ver/editar flujo trámite radicado",
     "desc": "Descripción"};
 
 data["22"] = {
     "file": "22-functionary-procedure-filter.png",
-    "desc": "Descripción"};
-
-data["23"] = {
-    "file": "22-functionary-procedure-filter.png",
+    "title": "Filtrar trámites por radicado o rango de fechas",
     "desc": "Descripción"};
 
 data["23"] = {
     "file": "23-functionary-procedure-approval.png",
+    "title": "Aprovar trámite",
     "desc": "Descripción"};
 
 data["24"] = {
     "file": "24-citizen-dashboard.png",
+    "title": "Dashboard ciudadano",
     "desc": "Descripción"};
 
 data["25"] = {
     "file": "25-citizen-dashboard-alert.png",
+    "title": "Dashboard notificación ciudadano",
     "desc": "Descripción"};
 
 data["26"] = {
     "file": "26-citizen-complete-procedure-form.png",
+    "title": "Iniciar trámite",
     "desc": "Descripción"};
 
 data["27"] = {
     "file": "27-citizen-upload-requirements-form.png",
+    "title": "Subir archivos de requerimientos",
     "desc": "Descripción"};
 
 data["28"] = {
     "file": "28-citizen-current-procedures-list.png",
+    "title": "Listar trámites en curso",
     "desc": "Descripción"};
 
 data["29"] = {
     "file": "29-citizen-history-procedures-list.png",
+    "title": "Listar histórico de trámites",
     "desc": "Descripción"};
 
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,33 @@ $(window).resize(function() {
     $("#accordion").accordion("resize");
 });
 
+mockContainer = $("#mock");
+
 $("a").on("click", function(ev) {
 
     var path = "assets/img/";
 
     src = $(this).data("source");
 
-    mockContainer = $("#mock");
     mockContainer.empty();
 
     img = $("<img class='pure-img' />");
     img.attr("src", path + data[src].file);
 
+    desc = $("<div class='desc'></div>");
+    desc.html(data[src].desc).hide();;
+
+    descTitle = $("<h3 class='desc-title'></h3>");
+    descTitle.html(data[src].title);
+
+    desc.prepend(descTitle);
+
     mockContainer.append(img);
+    mockContainer.append(desc);
+});
+
+mockContainer.on('mouseenter', function(ev) {
+    $(".desc").show();
+}).on('mouseleave', function(ev) {
+    $(".desc").hide();
 });
