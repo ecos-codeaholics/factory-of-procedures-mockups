d8e4c498521f3d65567f129cbf02944003a0ce23
andres0sorio <osorio.af@gmail.com>
2016-07-25 17:23:38 -0500

    [added] Description for each mockup added. In sync with documented, User Cases.

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,157 +3,154 @@ var data = [];
 data["00"] = {
     "file": "00-user-login.png",
     "title": "Login",
-    "desc": "Descripcion" };
+    "desc": "Atiende el requeriemiento de seguridad, usuario debe estar registrado e ingresa al sistema." };
 
 data["000"] = {
     "file": "000-user-signup.png",
     "title": "Login",
-    "desc": "Descripcion" };
+    "desc": "CU.C1: Registro de Usuarios." };
 
 data["01"] = {
     "file": "01-super-admin-dashboard.png",
     "title": "Dashboard",
-    "desc": "Descripcion" };
+    "desc": "Vista principal (home) del superusuario." };
 
 data["02"] = {
     "file": "02-super-admin-mayoralty-list.png",
     "title": "Listar alcaldías",
-    "desc": "Descripción"}
+    "desc": "CU.SA1: Listar Alcaldias. En la sección alcaldías, el usuario podrá visualizar una lista de las alcaldías creadas en el sistema ordenadas a través de una tabla."}
 
 data["03"] = {
     "file": "03-super-admin-mayoralty-new.png",
     "title": "Crear alcaldía",
-    "desc": "Descripción"};
+    "desc": "CU.SA2: Crear Alcaldias. El usuario deberá diligenciar la información de los campos del formulario de creación Alcaldía."};
 
 data["04"] = {
     "file": "04-super-admin-procedure-list.png",
     "title": "Listar trámites",
-    "desc": "Descripción"};
+    "desc": "CU.SA4: Listar Trámites. En la sección trámites, el usuario podrá visualizar la lista de los trámites de todo el sistema."};
 
 data["05"] = {
     "file": "05-super-admin-procedure-form-builder.png",
     "title": "Crear trámite (FormBuilder)",
-    "desc": "Descripción"};
+    "desc": "CU.SA5: Crear Trámite. La creación del trámite cuenta con dos subsecciones: Constructor del formulario (FormBuilder) y Constructor de Flujo (FlowBuilder)."};
 
 data["06"] = {
     "file": "06-super-admin-procedure-form-builder-attribs.png",
     "title": "Crear trámite (FormBuilder, Field Attributes)",
-    "desc": "Descripción"};
+    "desc": "CU.SA5 (FormBuilder)."};
 
 data["07"] = {
     "file": "07-super-admin-procedure-form-builder-validate.png",
     "title": "Crear trámite, Field Validate",
-    "desc": "Descripción"};
+    "desc": "CU.SA5 (FormBuilder)."};
 
 data["08"] = {
     "file": "08-super-admin-procedure-flow-builder-activities.png",
     "title": "FlowBuilder, Activities",
-    "desc": "Descripción"};
+    "desc": "CU.SA5 (FlowBuilder)."};
 
 data["09"] = {
     "file": "09-super-admin-procedure-flow-builder-assignee.png",
     "title": "FlowBuilder, Assignment",
-    "desc": "Descripción"};
+    "desc": "CU.SA5 (FlowBuilder)."};
 
 data["10"] = {
     "file": "10-super-admin-user-role-set.png",
     "titiel": "Asignar privilegios a usuarios",
-    "desc": "Descripción"};
+    "desc": "CU.SA8: En la sección de usuarios, el administrador puede consultar un usuario existente en el sistema por cédula y asignarle el rol de administrador y asociarlo a una alcaldía existente."};
 
 data["11"] = {
     "file": "11-super-admin-user-list-admins.png",
     "title": "Listar usuarios administradores",
-    "desc": "Descripción"};
+    "desc": "CU.SA9:En la sección de usuarios el administrador puede listar los usuarios administradores existentes en el sistema."};
 
 data["12"] = {
     "file": "12-admin-dashboard.png",
     "title": "Dashboard administrador",
-    "desc": "Descripción"};
+    "desc": "Dashboard del Administrador (municipio)."};
 
 data["13"] = {
     "file": "13-admin-mayoralty-edit.png",
     "title": "Editar Alcaldía",
-    "desc": "Descripción"};
+    "desc": "CU.AF9: El Administrador puede modificar alguna información básica del Municipio, mas no borrarla."};
 
 data["14"] = {
     "file": "14-admin-procedures-list.png",
     "title": "Listar trámites pendientes",
-    "desc": "Descripción"};
+    "desc": "CU.AF1: Un funcionario podrá consultar todos los trámites que tiene pendientes o los que tiene pendientes la secretaría a la que pertenece."};
 
 data["15"] = {
     "file": "15-admin-user-list-functionary.png",
     "title": "Listar funcionarios alcaldia",
-    "desc": "Descripción"};
+    "desc": "CU.AF10: El Administrador puede listar la información básica de los funcionarios."};
 
 data["16"] = {
     "file": "16-functionary-dashboard.png",
     "title": "Dashboard funcionario",
-    "desc": "Descripción"};
+    "desc": "Dashboard del Funcionario de la Alcaldia."};
 
 data["16.1"] = {
     "file": "16.1-functionary-dashboard-notification.png",
     "title": "Dashboard funcionario, notificación de actividad",
-    "desc": "Descripción"};
+    "desc": "Dashboard funcionario, ejemplo de notificación de actividad."};
 
 data["17"] = {
     "file": "17-functionary-procedure-pending-list.png",
     "title": "Listar trámites pendientes",
-    "desc": "Descripción"};
+    "desc": "CU.AF1: Un funcionario podrá consultar todos los trámites que tiene pendientes o los que tiene pendientes la secretaría a la que pertenece."};
 
 data["18"] = {
     "file": "18-functionary-procedure-review-info.png",
     "title": "Ver información trámite radicado",
-    "desc": "Descripción"};
+    "desc": "CU.AF3: Un funcionario puede seleccionar un trámite, y consultar la información suministrada por el ciudadano."};
 
 data["19"] = {
     "file": "19-functionary-procedure-review-requirements.png",
     "title": "Ver requerimientos trámite radicado",
-    "desc": "Descripción"};
+    "desc": "CU.AF4: Un funcionario puede seleccionar un trámite, y consultar los documentos  suministrados por el ciudadano."};
 
 data["20"] = {
     "file": "20-functionary-procedure-review-flow.png",
     "title": "Ver/editar flujo trámite radicado",
-    "desc": "Descripción"};
+    "desc": "CU.AF6:Un funcionario puede seleccionar un trámite, y consultar el flujo de secretarias por el que debe pasar el trámite para ser procesado."};
 
 data["22"] = {
     "file": "22-functionary-procedure-filter.png",
     "title": "Filtrar trámites por radicado o rango de fechas",
-    "desc": "Descripción"};
+    "desc": "CU.AF2: Un funcionario puede seleccionar un trámite, y consultar el historial."};
 
 data["23"] = {
     "file": "23-functionary-procedure-approval.png",
-    "title": "Aprovar trámite",
-    "desc": "Descripción"};
+    "title": "Aprobar trámite",
+    "desc": "CU.AF8: Un funcionario puede aprobar el paso del trámite por su secretaria, con esto el trámite pasará a otra secretaría o finalizara todo el proceso."};
 
 data["24"] = {
     "file": "24-citizen-dashboard.png",
     "title": "Dashboard ciudadano",
-    "desc": "Descripción"};
+    "desc": "Dashboard ciudadano."};
 
 data["25"] = {
     "file": "25-citizen-dashboard-alert.png",
     "title": "Dashboard notificación ciudadano",
-    "desc": "Descripción"};
+    "desc": "Dashboard, ejemplo de nueva notificación al ciudadano."};
 
 data["26"] = {
     "file": "26-citizen-complete-procedure-form.png",
     "title": "Iniciar trámite",
-    "desc": "Descripción"};
+    "desc": "CU.C3: Solicitar tramites."};
 
 data["27"] = {
     "file": "27-citizen-upload-requirements-form.png",
     "title": "Subir archivos de requerimientos",
-    "desc": "Descripción"};
+    "desc": "CU.C7: Subir archivos de requerimientos."};
 
 data["28"] = {
     "file": "28-citizen-current-procedures-list.png",
     "title": "Listar trámites en curso",
-    "desc": "Descripción"};
+    "desc": "CU.C4: Consultar estado actual de un trámite solicitado. El ciudadano podrá consultar el estado de cualquier trámite en proceso que haya solicitado."};
 
 data["29"] = {
     "file": "29-citizen-history-procedures-list.png",
     "title": "Listar histórico de trámites",
-    "desc": "Descripción"};
-
-
-
+    "desc": "CU.C6: Consultar historico de tramites. El ciudadano podrá consultar el histórico de trámites solicitados."};
